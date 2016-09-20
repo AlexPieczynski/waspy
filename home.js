d218c971caaaa1c33014dfcf711592ba470d456a
Alex Pieczynski <apiecz2@uic.edu>
2016-09-20 15:04:17 -0500

    Added code to embed a youtube link   - also added code to play a local mp4 video file   - the code currently looks for a file "poppy.mp4" on the server

--- home.js
+++ home.js
@@ -1,5 +1,20 @@
 function submitButton()
 {
 	var videoURL = document.getElementById("videoURL").value;
-	window.location.href = videoURL;
+	videoURL = videoURL.replace("watch?v=","embed/");
+	document.getElementById("youtubeDiv").setAttribute("style", "");
+	document.getElementById("youtubeVid").setAttribute("src", videoURL);
+}
+
+function playLocal()
+{
+	document.getElementById("vidPlayer").setAttribute("style", "");
+	document.getElementById("vid").play();
+}
+
+function fileUpload()
+{
+	var file = document.getElementById("fileUpload").value;
+	var audio = new Audio('test.mp3');
+	audio.play();
 }
